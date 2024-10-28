40aa0e900dd035a3fc550dfd3eedab8068f42d36
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 13:32:03 +0000

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_line - Entry point
+ *@c : braga
+ *
+ *
+ *   Return: Always 0 (Success)
+ *
+*/
+
+void print_line(int n)
+{
+	int n, s;
+
+	if (n < 0)
+		continue;
+	s = n * '_'
+		_putchar("%c", s)
+}
