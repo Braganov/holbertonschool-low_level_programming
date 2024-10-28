02a6effc7ae6ca278726a5229aff412734bceee4
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 12:55:53 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  *more_numbers - Entry point
- *@c : braga
+ *
  *
  *
  *   Return: Always 0 (Success)
@@ -12,7 +12,8 @@
 void more_numbers(void)
 {
 	int i;
-	char tobeprinted[20] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 49, 48, 49, 49, 49, 50, 49, 51, 49, 52};
+	char tobeprinted[20] = {48, 49, 50, 51, 52, 53, 54,
+ 55, 56, 57, 49, 48, 49, 49, 49, 50, 49, 51, 49, 52};
 
 	for (i = 0; i < 10; i++)
 	{
