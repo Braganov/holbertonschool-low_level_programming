8c67b8a47b8e7e48dbbcf6de139c0162b6728dc4
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 12:46:02 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,25 +12,19 @@
 void more_numbers(void)
 {
 	int i;
+	char tobeprinted[20] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 49, 48, 49, 49, 49, 50, 49, 51, 49, 52};
 
 	for (i = 0; i <= 10; i++)
-{
 	{
-		int main(void)
-{
-		char tobeprinted[19] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4};
-		int a = 0
+		int a = 0;
 
-		while (a < 19)
+		while (a < 20)
 		{
 		_putchar(tobeprinted[a]);
 		a++;
 		}
 		_putchar(10);
-		return (0);
- 		}
 	}
 	_putchar('\n');
-}
 }
 
