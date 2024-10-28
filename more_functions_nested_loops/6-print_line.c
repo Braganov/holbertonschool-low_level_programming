e7c42b696e2214e8ec68f16f99161b31320cfbe5
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 13:51:10 +0000

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- *print_line - Entry point
- *@c : braga
+ * print_line - Write a function that draws a straight line in the terminal.
  *
+ * @n: number of times the character _ should be printed
  *
- *   Return: Always 0 (Success)
- *
-*/
+ */
 
 void print_line(int n)
 {
