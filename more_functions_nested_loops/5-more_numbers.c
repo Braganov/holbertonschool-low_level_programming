c7b8b3fa8f26d8ce89879357cb27f21d38078711
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 12:02:30 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_numbers - Entry point
+ *
+ *
+ *
+ *   Return: Always 0 (Success)
+ *
+*/
+
+void more_numbers(void)
+{
+	int m , i;
+
+	for ( i=0 ; i<=10 ; i++)
+{
+	{
+		for ( m = 0 ; m<= 14 ; m++)
+
+		_putchar (i + '0');
+	}
+	_putchar ('\n');
+}
+}
+
