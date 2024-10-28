607582d8a0771bb6936dec22434ca78aa5fb6069
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 09:40:01 +0000

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - Entry point
+ *@c : braga
+ *
+ *
+ *Return: Always 0 (Success)
+ *
+*/
+
+int _isdigit(int c)
+{
+		return (c >= '0' && c <= '9');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _isdigit(int c);
 void print_to_98(int n);
 int add(int, int);
 void times_table(void);
