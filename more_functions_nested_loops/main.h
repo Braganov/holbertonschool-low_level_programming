d14246eaccab011fc4edf9ea257de7fb18df5b24
Braga <ibrahim.houmaidi@gmail.com>
2024-10-28 09:28:56 +0000

    0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isupper(int c);
+
+#endif
