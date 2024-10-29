63953c0b6ec0a48c4747d260d6214a34d4d4a130
Braga <ibrahim.houmaidi@gmail.com>
2024-10-29 09:05:34 +0000

    0

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isupper(int c);
+
+#endif
