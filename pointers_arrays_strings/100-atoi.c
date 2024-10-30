97a87a8f1cfdce31297ee066971c15e77f8ba05b
Braga <ibrahim.houmaidi@gmail.com>
2024-10-30 13:03:03 +0000

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _atoi - Convert a string to an integer.
  *
@@ -8,26 +9,29 @@
  * Return: integer
  */
 
-int  _atoi(char *s)
+int _atoi(char *s)
 {
-	int sign, result, index;
+    int sign = 1;
+    int result = 0;
+    int index = 0;
 
-	result = 0;
-	sign = 0;
+    // تحديد علامة الرقم (موجب أو سالب)
+    while (s[index] != '\0' && (s[index] < '0' || s[index] > '9'))
+    {
+        if (s[index] == '-')
+        {
+            sign *= -1;  // تغيير العلامة إذا تم العثور على علامة سالب
+        }
+        index++;
+    }
 
-	for (index = 0; s[index] != '\0'; index++)
-	{
-		if (s[index] == '-')
-		{
-			sign++;
-		}
-		if (s[index] > 47 && s[index] < 58)
-		{
-			while (s[index] > 47 && s[index] < 58)
+    // تحويل الأرقام إلى عدد صحيح
+    while (s[index] >= '0' && s[index] <= '9')
+    {
+        result = result * 10 + (s[index] - '0'); // تحويل الحرف إلى رقم وإضافته للنتيجة
+        index++;
+    }
 
-				result = result * 10 - (s[index++] - 48);
-				break;
-		}
-	}
-	return (result *= sign % 2 == 0 ? -1 : 1);
+    return result * sign; // تعيين العلامة النهائية للنتيجة
 }
+
