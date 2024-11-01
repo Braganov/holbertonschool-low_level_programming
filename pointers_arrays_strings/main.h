ca4a186090a2f676980919edda24d02d3bca0c93
Braga <ibrahim.houmaidi@gmail.com>
2024-11-01 13:00:11 +0000

    3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,41 +1,29 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef INV_MAIN_H
+#define INV_MAIN_H
 
-#include <stdio.h>
-
-int _atoi(char *s);
-char *_strcpy(char *dest, char *src);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-void rev_string(char *s);
-void print_rev(char *s);
-int _strlen(char *s);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
-void print_line(int n);
-void more_numbers(void);
-void print_most_numbers(void);
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
-void print_to_98(int n);
-int add(int, int);
-void times_table(void);
-void jack_bauer(void);
-int print_last_digit(int);
-int _abs(int);
-int print_sign(int n);
-int _isalpha(int c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _putchar(char c);
-int _islower(int c);
-int _isupper(int c);
+int _putchar(char);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
