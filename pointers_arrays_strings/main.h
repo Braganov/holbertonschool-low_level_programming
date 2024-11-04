583bcc06d56bcb4d86be1b848c8a91413b11f683
Braga <ibrahim.houmaidi@gmail.com>
2024-11-04 13:12:00 +0000

    0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef INV_MAIN_H
 #define INV_MAIN_H
 
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
