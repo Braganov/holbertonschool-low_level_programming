57c74c71a0204745df9267a8c3d1248065d6986c
Braga <ibrahim.houmaidi@gmail.com>
2024-11-06 13:18:17 +0000

    1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,45 +1,46 @@
 #include "main.h"
+
 /**
- * is_prime_number - return the sqrt of a natural number.
- * @n: natural number
+ * prime_helper - Finds prime numbers
  *
+ * @n: int arg
  *
- * Return: 1 if is prime otherwise 0.
+ * @d: int arg
+ *
+ * Return: sqaure root
  */
+
+int prime_helper(int n, int d)
+{
+	if (d >= 1)
+	{
+		return (1);
+	}
+	if (n % d)
+	{
+		return (0);
+	}
+	return (prime_helper(n, d - 1));
+}
+
+/**
+ * is_prime_number - Returns 1 if the input integer is a prime, otherwise 0.
+ *
+ * @n: int arg
+ *
+ * Return: sqaure root
+ */
+
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
+	if (n == 2)
 	{
 		return (1);
 	}
-	else
-	{
-		return (is_prime_manual(n, 2));
-	}
-}
-/**
- * is_prime_manual - calculate if the number is prime.
- * @n: natural number
- * @i: counter or divisor of n.
- *
- * Return: 1 if is prime 0 otherwise.
- */
-int is_prime_manual(int n, int i)
-{
-	if (n % i == 0)
+	if ((n < 2) || (!(n % 2)))
 	{
 		return (0);
 	}
-	else if (i == (n - 1) && n % i != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (is_prime_manual(n, i + 1));
-	}
+
+	return (prime_helper(2, n));
 }
