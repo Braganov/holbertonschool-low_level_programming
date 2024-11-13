3c53a98d5c5592b3f8a190fc09339fb01b317dcc
Braga <ibrahim.houmaidi@gmail.com>
2024-11-13 09:58:08 +0100

    0.1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,28 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * _strdup- copy a string.
- * @str: string to be copied.
- *create a duplicate of a string 
+ * *_strdup - check the code.
  *
- * Return: pointer to string or null if faillure
+ * Create a duplicate of a string
+ *
+ *
+ * @str: the string to copy
+ * Return: Pointer to string or null if faillure
  */
 char *_strdup(char *str)
 {
-	int strSize, i;
-	char *copyStr;
+	char *newstr;
+	int size = 0;
 
-	strSize = 0;
-	if (str == NULL)
+	if (str == 0)
 		return (NULL);
-	while (*(str + strSize))
-		strSize++;
-	strSize++; /* add 1 to print NULL*/
-	copyStr = malloc((strSize) * sizeof(char));
-	if (copyStr == NULL)
+
+	for (; str[size] != '\0';)
+		size++;
+	size++;
+
+	newstr = malloc(size);
+	if (newstr == NULL)
 		return (0);
-	for (i = 0; i < (strSize); i++)
-		*(copyStr + i) = *(str + i);
-	return (copyStr);
+
+	for (; size >= 0; size--)
+		newstr[size] = str[size];
+
+	return (newstr);
 }
