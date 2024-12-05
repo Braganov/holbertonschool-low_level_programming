76ff97623d3b8c288606e18af74412beb1be80ea
Braga <ibrahim.houmaidi@gmail.com>
2024-12-05 19:45:14 +0100

    fix betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,33 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
- * @head: double pointer to the list_t list.
- * @str: the string to put in the new node.
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to add as the new node's str
  *
- * Return: the address of the new element, or NULL if it failed.
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
+list_t *new_node;
+size_t i;
 
-if (!str) // Vérification si str est NULL
+if (!str)
 return (NULL);
-
-new = malloc(sizeof(list_t));
-if (!new)
+new_node = malloc(sizeof(list_t));
+if (!new_node)
 return (NULL);
-
-new->str = strdup(str);
-if (!new->str) // Vérification si strdup échoue
+new_node->str = strdup(str);
+if (!new_node->str)
 {
-free(new);
+free(new_node);
 return (NULL);
 }
-
-new->len = strlen(str);
-new->next = *head;
-*head = new;
-
-return (new);
+for (i = 0; str[i] != '\0'; i++)
+;
+new_node->len = i;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
- * @head: double pointer to the list_t list.
- * @str: string to be added to the new node.
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to add as the new node's str
  *
- * Return: address of the new element, or NULL if it failed.
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *temp;
+list_t *new_node, *temp;
+size_t i;
 
-    new = malloc(sizeof(list_t));
-    if (!new)
-        return (NULL);
-
-    new->str = strdup(str);
-    if (!new->str)
-    {
-        free(new);
-        return (NULL);
-    }
-
-    new->len = strlen(str);
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = new;
-    }
-
-    return (new);
+temp = *head;
+if (!str)
+return (NULL);
+new_node = malloc(sizeof(list_t));
+if (!new_node)
+return (NULL);
+new_node->str = strdup(str);
+if (!new_node->str)
+{
+free(new_node);
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+;
+new_node->len = i;
+new_node->next = NULL;
+if (!*head)
+{
+*head = new_node;
+}
+else
+{
+while (temp->next)
+temp = temp->next;
+temp->next = new_node;
+}
+return (new_node);
 }
